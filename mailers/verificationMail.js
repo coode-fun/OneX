cca7a3dda395ec66331cd43e2dd4c7ccf2f582e4
coode-fun <70048122+coode-fun@users.noreply.github.com>
2021-01-24 14:27:21 +0530

    student login part done

--- mailers/verificationMail.js
+++ mailers/verificationMail.js
@@ -7,7 +7,7 @@ exports.newVerification=(user)=>{
         from:'oneX.sas.2021@gmail.com',
         to:user.email,
         subject:"This is subject",
-        html:`<h1>Yup, your comment has been publshed!!<h1> <br> <a href="http://localhost:5000/students/verify/${user.email}">CLICK TO ACTIVATE</a>`
+        html:`<h1>Yup, your comment has been publshed!!<h1> <br> <a href="http://onex-system.herokuapp.com/students/verify/${user.email}">CLICK TO ACTIVATE</a>`
     },(err,info)=>{
         if(err){
             console.log("Error in sending mail :",err);
